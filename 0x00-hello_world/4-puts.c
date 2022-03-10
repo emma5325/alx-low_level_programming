bc2a3e982b745791c98b3cd1a11754c6b7496372
emma5325 <jerryemma560@gmail.com>
2022-03-10 05:26:07 -0800

    my new task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-1   #include <stdio.h>
-2
-3  /**
-4   * main - prints the phrase "Programming is like building a multilingual puzzle
-5   * Return: 0 if exited properly, non-zero otherwise
-6   */
-7   int main(void)
-8   { 
-9       puts("\"Programming is like building a multilingual puzzle");
-10     return (0);
-11  } 
+   #include <stdio.h>
+
+  /**
+   * main - prints the phrase "Programming is like building a multilingual puzzle
+   * Return: 0 if exited properly, non-zero otherwise
+   */
+   int main(void)
+   { 
+       puts("\"Programming is like building a multilingual puzzle");
+     return (0);
+   } 
