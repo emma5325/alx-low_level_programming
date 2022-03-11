e6898aeb6b411cab906eeabc6d7a63f639eebcf3
emma5325 <jerryemma560@gmail.com>
2022-03-11 06:08:36 -0800

    my new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,15 +5,15 @@
  */
 int main(void)
 {    char c;
-     int i;
-     long l;
-     long long ll;
-     float f;
-     printf("Size of a char: %zu byte(s)\n", sizeof(c));
-     printf("Size of an int: %zu byte(s)\n", sizeof(i));
-     printf("Size of a long int: %zu byte(s)\n", sizeof(l));
-     printf("Size of a long long int: %zu byte(s)\n", sizeof(ll));
-    printf("Size of a float: %zu byte(s)\n", sizeof(f));
-    	return (0);
+ int i;
+ long l;
+ long long ll;
+ float f;
+ printf("Size of a char: %zu byte(s)\n", sizeof(c));
+ printf("Size of an int: %zu byte(s)\n", sizeof(i));
+ printf("Size of a long int: %zu byte(s)\n", sizeof(l));
+ printf("Size of a long long int: %zu byte(s)\n", sizeof(ll));
+ printf("Size of a float: %zu byte(s)\n", sizeof(f));
+    return (0);
 }
 		   
