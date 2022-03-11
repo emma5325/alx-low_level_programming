6b430b625cafc824f0c919756f63f12ffe59b0a5
emma5325 <jerryemma560@gmail.com>
2022-03-11 05:24:32 -0800

    my second commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point 
- * Return: Always 0 (success)
+ *  main - Entry point
+ *  Return: Always 0 (Success)
 */
-int main(void)
-{    puts("Programming is like building a multilingual puzzle");
+int main (void)
+{    printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
