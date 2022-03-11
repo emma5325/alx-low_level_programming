499f292785e58761a9afb5874c9f9fae5c0cddde
emma5325 <jerryemma560@gmail.com>
2022-03-11 06:36:10 -0800

    my new commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -4,11 +4,9 @@
  * Return: Always 0 (Success)
 */
 int main(void)
-{ char x;
-for (x = '0'; x <= '9'; x++)
-putchar(x);
-for (x = 'a'; x <= 'f'; x++)
-     putchar(x);	
+{ char x;for (x = '0'; x <= '9'; x++)
+putchar(x);for (x = 'a'; x <= 'f'; x++)
+putchar(x);	
 putchar('\n');
 return (0);
 }
