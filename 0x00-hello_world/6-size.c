13b8c502d48a380a7602a4185607af572cdc70cc
emma5325 <jerryemma560@gmail.com>
2022-03-11 06:02:10 -0800

    my commit on this task

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
-
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
-{
-irntf("Size of a char: %d byte(s)\n", sizeof(char));
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-printf("Size of a float: %d byte(s)\n", sizeof(float));
-return (0);
+{    char c;
+     int i;
+     long l;
+     long long ll;
+     float f;
+     printf("Size of a char: %zu byte(s)\n", sizeof(c));
+     printf("Size of an int: %zu byte(s)\n", sizeof(i));
+     printf("Size of a long int: %zu byte(s)\n", sizeof(l));
+     printf("Size of a long long int: %zu byte(s)\n", sizeof(ll));
+    printf("Size of a float: %zu byte(s)\n", sizeof(f));
+    	return (0);
 }
-
-
-
-
+		   
