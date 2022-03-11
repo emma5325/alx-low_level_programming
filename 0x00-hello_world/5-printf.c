9ef64a258f42604e3f03a058d4aee9f3c8c0d395
emma5325 <jerryemma560@gmail.com>
2022-03-11 05:34:51 -0800

    my new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- *  main - Entry point
- *  Return: Always 0(Success)
+ *main - Entry point
+ *Return: Always 0 (Success)
 */
 int main (void)
 {    printf("with proper grammar, but the outcome is a piece of art,\n");
