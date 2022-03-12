a2c5bfd93b01011797c7ae7bd2f5f7b4d77e7e9a
emma5325 <jerryemma560@gmail.com>
2022-03-11 22:23:07 -0800

    my new comit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+*/
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+putchar(x);
+for (x = 'A'; x <= 'Z'; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
