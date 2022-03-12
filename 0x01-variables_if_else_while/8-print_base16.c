2cc79b66990c7197da580bb6047a87ab9d412caa
emma5325 <jerryemma560@gmail.com>
2022-03-12 01:46:28 -0800

    my new commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
- *main - Entry point
- *  
+ *main - Entry point  
  *Return: Always 0 (Success)
  */
 int main(void)
 {
 char x;
-
 for (x = '0'; x <= '9'; x++)
 putchar(x);
 for (x = 'a'; x <= 'f'; x++)
