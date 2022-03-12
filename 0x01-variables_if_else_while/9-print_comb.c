66594b58586a63e810f990a81608049f6c7a86f2
emma5325 <jerryemma560@gmail.com>
2022-03-11 23:47:51 -0800

    my new commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+putchar((x % 10) + '0');
+if (x == 9)
+continue;
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
