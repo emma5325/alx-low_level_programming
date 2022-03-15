f27843a9d32c7b5ab6a26adb387d7429ebeff998
emma5325 <jerryemma560@gmail.com>
2022-03-15 08:30:37 -0700

    my new commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,31 +1,17 @@
 #include "main.h"
 /**
- *times_table - Prints array of multiplication tables starting from 0
- *values in 'putchar' are given in ASCII format codes
- *Return: 9 times tables
+ *This function prints the pharse '_putchar' to the StndOut
+ *The variable 'a' sets counter from 0
+ *The variable 'b' defines an array of the letters that are going to be written
+ *Return: On success 1.
  **/
-void times_table(void)
+int main(void)
 {
-int x, y, z;
-for (x = 0; x <= 9; x++)
-}
-_putchar(48);
-for (y = 1; y <= 9; y++)
+int a = 0;
+char b[] = "_putchar/n";
+for (a = 0; a < 11; a++)
 {
-z = x * y;
-_putchar(44);
-_putchar(32);
-if (z <= 9)
-{
-_putchar(32);
-_putchar(z + 48);
+_putchar(b[a]);
 }
-else
-{
-_putchar((z / 10) + 48);
-_putchar((z % 10) + 48);
-}
-}
-_putchar('\n');
-}
+return (0);
 }
