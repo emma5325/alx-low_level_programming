1253ac61996a8be5030afae05c5523f29d3792fb
emma5325 <jerryemma560@gmail.com>
2022-03-16 05:11:48 -0700

    my new commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,11 @@
 #include <stdio.h>
 
 /**
- *  * _abs - value absolute
- *   *
- *    *@n: The int to print
- *     * Return: Always 0.
- *      */
-
+ *_abs - value absolute
+ *
+ *@n: The int to print
+ *Return: Always 0.
+ */
 int _abs(int n)
 {
 
