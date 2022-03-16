cc292221039350062a17fdce72d38b83b0007cad
emma5325 <jerryemma560@gmail.com>
2022-03-16 02:02:55 -0700

    my new commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return: Always 0
+ *main - Entry point
+ *Return: nothing, return void
  */
 int main(void)
 {
-_putchar('-');
+_putchar('_');
 _putchar('p');
 _putchar('u');
 _putchar('t');
