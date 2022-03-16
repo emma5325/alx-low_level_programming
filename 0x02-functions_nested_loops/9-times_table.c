21df9f6e0d3c8a15d6bf7478d9be6336ddf2bcb8
emma5325 <jerryemma560@gmail.com>
2022-03-16 03:29:48 -0700

    my new commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,17 +1,22 @@
 #include "main.h"
 /**
- *This function prints the pharse '_putchar' to the StndOut
- *The variable 'a' sets counter from 0
- *The variable 'b' defines an array of the letters that are going to be written
- *Return: On success 1.
- **/
-int main(void)
+ * jack_bauer -prints every minute of the day
+ * Return: Always 0.
+ */
+void jack_bauer(void)
 {
-int a = 0;
-char b[] = "_putchar/n";
-for (a = 0; a < 11; a++)
+int a;
+int b;
+for (a = 0; a <= 23; a++)
 {
-_putchar(b[a]);
+for (b = 0; b <= 59; b++)
+{
+_putchar (a / 10 + '0');
+_putchar (a % 10 + '0');
+_putchar (':');
+_putchar (b / 10 + '0');
+_putchar (b % 10 + '0');
+_putchar ('\n');
 }
-return (0);
+}
 }
