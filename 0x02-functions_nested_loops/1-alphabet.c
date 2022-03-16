58affec69242e302487fa1d646a5aab4c8bd91d3
emma5325 <jerryemma560@gmail.com>
2022-03-16 00:45:43 -0700

    my new commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Return: void
+ */
+int main(void)
+{
+void print_alphabet(void);
+char i;
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ * Description: print lowercase alphabet followed by newline 10 times
+ * Return: void
+ */void print_alphabet_x10(void)
+{
+int i;
+int c;
+for (i = 0; i < 10; ++i)
+{
+for (c = 'a'; c <= 'z'; ++c)
+_putchar(c);
+_putchar('\n');
+}
+}
