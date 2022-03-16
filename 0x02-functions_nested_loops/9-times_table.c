4f9792e631c67c73a2907fa1176f4411b4ed05e8
emma5325 <jerryemma560@gmail.com>
2022-03-16 03:39:25 -0700

    my new commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,22 +1,32 @@
 #include "main.h"
 /**
- * jack_bauer -prints every minute of the day
- * Return: Always 0.
+ *times_table -prints tables
+ * Return: Always 0
  */
-void jack_bauer(void)
+void times_table(void)
 {
 int a;
 int b;
-for (a = 0; a <= 23; a++)
+int c;
+for (a = 0; a <= 9; a++)
 {
-for (b = 0; b <= 59; b++)
+for (b = 0; b <= 9; b++)
 {
-_putchar (a / 10 + '0');
-_putchar (a % 10 + '0');
-_putchar (':');
-_putchar (b / 10 + '0');
-_putchar (b % 10 + '0');
-_putchar ('\n');
+c = a * b;
+if ((c / 10) == 0)
+{
+if (b == 0)
+{
+_putchar ('0');
 }
+if (b != 0)
+{
+_putchar (' ');
+_putchar ((c % 10) + '0');
+}
+if (b < 9)
+{
+_putchar(',');
+_putchar (' ');
 }
 }
