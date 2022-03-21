b11ff7802367c85917ba54e9418d79c7e7edb564
emma5325 <jerryemma560@gmail.com>
2022-03-21 05:37:44 -0700

    my new commit today

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
-
-void reset_to_98(int *);
+#include "main.h"
 /**
- * main - check the code for Holberton School students.
- * Return: Always 0.
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
  */
-int main(void)
+void reset_to_98(int *n)
 {
-int n;
-n = 402;
-printf("%d\n", n);
-reset_to_98(&n);
-printf("%d\n", n);
-return (0);
+*n = 98;
 }
+
