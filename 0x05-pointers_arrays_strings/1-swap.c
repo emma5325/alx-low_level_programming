85e0e29fcbb1439daf3ac5ef8fc0cc0d4e62d6d9
emma5325 <jerryemma560@gmail.com>
2022-03-21 05:56:56 -0700

    my new task today

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,17 +1,13 @@
 #include "main.h"
-#include <stdio.h>
 /**
- *main - check the code
- *Return: Always 0.
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
  */
-int main(void)
+
+void reset_to_98(int *n)
 {
-int a;
-int b;
-a = 98;
-b = 42;
-printf("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-printf("a=%d, b=%d\n", a, b);
-return (0);
+
+*n = 98;
+
 }
