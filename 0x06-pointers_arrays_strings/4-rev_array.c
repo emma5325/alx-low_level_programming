923ad1498ec62b68319280eaf72b0264b3c66997
emma5325 <jerryemma560@gmail.com>
2022-03-23 09:11:34 -0700

    my new task today

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -6,16 +6,18 @@
  * @n: number of elements of the array.
  * Return: no return.
  */
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
 {
-int i, j, temp;
-for (i = 0; i < n - 1; i++)
-{
-for (j = i + 1; j > 0; j--)
-{
-temp = *(a + j);
-*(a + j) = *(a + (j - 1));
-*(a + (j - 1)) = temp;
-}
-}
+	int i, j, temp;
+
+	for (i = 0; i < n - 1; i++)
+
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(a + j);
+			*(a + j) = *(a + (j - 1));
+			*(a + (j - 1)) = temp;
+		}
+	}
 }
