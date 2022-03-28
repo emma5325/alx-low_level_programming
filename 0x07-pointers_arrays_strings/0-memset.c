06129937fcee30f471287245bbec04b6d9d3cba8
emma5325 <jerryemma560@gmail.com>
2022-03-28 03:01:45 -0700

    my new commit today

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 /**
  * *_memset - fills memory with a constant byte
  * @s: memory area to be filled
@@ -9,6 +9,7 @@ nclude "main.h"
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
