8be16dd85e0ee12db4beca91d291f73b148d57e7
emma5325 <jerryemma560@gmail.com>
2022-04-21 05:11:40 -0700

    my new commit today

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,11 +14,16 @@ list_t *add_node(list_t **head, const char *str)
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
+
 	new->str = strdup(str);
+
 	for (nchar = 0; str[nchar]; nchar++)
 		;
+
 	new->len = nchar;
 	new->next = *head;
 	*head = new;
+
 	return (*head);
 }
+
