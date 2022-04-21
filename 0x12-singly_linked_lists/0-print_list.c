10e0b0a2b6f6bc2f1838b757a37acd9baea188dd
emma5325 <jerryemma560@gmail.com>
2022-04-21 05:03:35 -0700

    my new task today

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -4,13 +4,17 @@
  * @h: singly linked list.
  * Return: number of elements in the list.
  */
-size_t list_len(const list_t *h)
+size_t print_list(const list_t *h)
 {
 	size_t nelem;
 
 	nelem = 0;
 	while (h != NULL)
 	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 		nelem++;
 	}
